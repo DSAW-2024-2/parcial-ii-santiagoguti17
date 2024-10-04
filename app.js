8e423fc6bd7786ff23ab6d7ad5932dea4e06806a
santiagogutierrez <santiagoguba@unisabana.edu.co>
2024-10-04 08:11:46 -0500

    app

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const authRoutes = require('./routes/auth');
 const weatherRoutes = require('./routes/weather');
 require('dotenv').config();
 
+// Validar que la clave secreta y el puerto estén definidos en el archivo .env
+if (!process.env.SECRET_KEY) {
+  throw new Error('Falta la clave secreta en las variables de entorno (SECRET_KEY)');
+}
 
+if (!process.env.PORT) {
+  console.warn('El puerto no está definido en las variables de entorno. Usando el puerto por defecto: 3000');
+}
 
 // Middlewares
 app.use(express.json());
-
 
 // Rutas
 app.use('/login', authRoutes);
